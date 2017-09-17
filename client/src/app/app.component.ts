78cf3ec639ff7288009a3467e8e5c170f5df3e0c
JimanPark <jiman.park@sk.com>
2017-09-18 00:46:29 +0900

    [TASK] build configuration

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { ipcRenderer } from 'electron';
+import 'fs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app';
+
+  constructor() {
+  }
+
+  ngAfterViewInit(): void {
+    console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "pong"
+
+    ipcRenderer.on('asynchronous-reply', (event, arg) => {
+      console.log(arg) // prints "pong"
+    })
+    ipcRenderer.send('asynchronous-message', 'ping')
+  }
 }
--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const electron = require('electron');
-const devTron = require('devtron').install();
+const electron      = require('electron');
+const { ipcMain }   = require('electron');
 // Module to control application life.
-const app = electron.app;
+const app           = electron.app;
 // Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
 // const menu = new electron.Menu();
 //
 // menu.append(new MenuItem({
@@ -12,15 +13,16 @@ const app = electron.app;
 // }))
 
 const path = require('path')
-const url = require('url')
+const url  = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-function createWindow () {
+function createWindow() {
+
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
 
   // and load the index.html of the app.
   // mainWindow.loadURL(url.format({
@@ -32,7 +34,7 @@ function createWindow () {
   mainWindow.loadURL(`file://${__dirname}/client/dist/index.html`);
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -52,7 +54,7 @@ app.on('ready', createWindow)
 app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+  if ( process.platform !== 'darwin' ) {
     app.quit()
   }
 })
@@ -60,10 +62,19 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
+  if ( mainWindow === null ) {
     createWindow()
   }
 })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+ipcMain.on('asynchronous-message', (event, arg) => {
+  console.log(arg)  // prints "ping"
+  event.sender.send('asynchronous-reply', 'pong')
+})
+
+ipcMain.on('synchronous-message', (event, arg) => {
+  console.log(arg)  // prints "ping"
+  event.returnValue = 'pong'
+})
