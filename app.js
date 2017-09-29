7cc9246f183ef2f439f8825bd7f14feb1bcc5504
JimanPark <jiman.park@sk.com>
2017-09-29 13:11:36 +0900

    [TASK] electron dev config

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow } = require('electron');
-
-const path = require('path')
-const url  = require('url')
+var client                   = require('electron-connect').client;
+const path                   = require('path')
+const url                    = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -9,10 +9,10 @@ let mainWindow;
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600, frame: true })
+  mainWindow = new BrowserWindow({ width: 800, height: 600, frame: true });
 
   // and load the index.html of the app.
-  mainWindow.loadURL('https://m.naver.com');
+  mainWindow.loadURL('http://localhost:4200');
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -24,6 +24,8 @@ function createWindow() {
     // when you should delete the corresponding element.
     mainWindow = null
   })
+
+  client.create(mainWindow);
 }
 
 // This method will be called when Electron has finished
